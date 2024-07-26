6918754a36f8afe0fe787bedb0b4a520c00ff88b
Apoorv Mittal <apoorvmitthu@gmail.com>
2024-07-27 00:40:46 +0530

    removed a comma, fixed alignment, and, added a heading and typewriter for meet the team

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
       <ToTop/>
   </BrowserRouter>
 )
