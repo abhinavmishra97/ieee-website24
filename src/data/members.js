285190c2ac123d54f286aa614594c93d46806f85
Apoorv Mittal <apoorvmitthu@gmail.com>
2024-07-28 13:06:28 +0530

    updated the paths of core members images

--- src/data/members.js
+++ src/data/members.js
@@ -1,45 +1,106 @@
-import abhinavSir from "../assets/core-team-members/Abhinav.jpg"
+import vimarshSir from "../assets/core-team-members/vimarsh_mishra.png";
+import devanshiMam from "../assets/core-team-members/Devanshi Mishra.jpeg";
+import krishnaSir from "../assets/core-team-members/Krishna_Agrawal.jpg";
+import gauravSir from "../assets/core-team-members/Gaurav_Agarwal.jpg";
+import pankhuriMam from "../assets/core-team-members/Pankhuri_Asthana.jpg";
+import muskaanSinghalMam from "../assets/core-team-members/Muskaan_Singhal.jpg";
+import hardikSir from "../assets/core-team-members/Hardik.jpg";
+import amanSir from "../assets/core-team-members/aman_singh.jpg";
+import nikhilSir from "../assets/core-team-members/NikhilMittal.jpg";
+import omarSir from "../assets/core-team-members/omar_shahab.jpg";
+import adityaSir from "../assets/core-team-members/aditya.jpg";
+import muskanAsthanaMam from "../assets/core-team-members/Muskan_Asthana.jpg";
+import suhaniMam from "../assets/core-team-members/Suhani Gupta.jpeg";
+import snehaMam from "../assets/core-team-members/Sneha_Tyagi.jpg";
+import kavyaMam from "../assets/core-team-members/Kavya Gauri.jpg";
+import abhinavSir from "../assets/core-team-members/Abhinav.jpg";
+import vanishaMam from "../assets/core-team-members/Vanisha Agarwal_.jpg";
 
 const members = [
   {
-    image: abhinavSir,
-    name: "John Doe",
-    post: "Team Leader",
+    image: vimarshSir,
+    name: "Vimarsh Sir",
+    post: "Chairperson",
+  },
+  {
+    image: devanshiMam,
+    name: "Devanshi Mishra",
+    post: "Vice Chairperson",
+  },
+  {
+    image: krishnaSir,
+    name: "Krishna Agrawal",
+    post: "Organising Secretary",
+  },
+  {
+    image: gauravSir,
+    name: "Gaurav Agarwal",
+    post: "Treasurer",
+  },
+  {
+    image: pankhuriMam,
+    name: "Pankhuri Asthana",
+    post: "WIE Head",
+  },
+  {
+    image: muskaanSinghalMam,
+    name: "Muskaan Singhal",
+    post: "Strategic Head",
+  },
+  {
+    image: hardikSir,
+    name: "Hardik Agarwal",
+    post: "Head Of Operations",
+  },
+  {
+    image: amanSir,
+    name: "Aman Singh",
+    post: "Technical Head CSE",
+  },
+  {
+    image: nikhilSir,
+    name: "Nikhil Mittal",
+    post: "Technical Head ECE",
+  },
+  {
+    image: omarSir,
+    name: "Omar Shahab",
+    post: "Digital Head",
+  },
+  {
+    image: adityaSir,
+    name: "Aditya Patil",
+    post: "Cinematography",
+  },
+  {
+    image: muskanAsthanaMam,
+    name: "Muskan Asthana",
+    post: "Marketing Head",
+  },
+  {
+    image: suhaniMam,
+    name: "Suhani Gupta",
+    post: "Public Relations Head",
+  },
+  {
+    image: snehaMam,
+    name: "Sneha Tyagi",
+    post: "Management Head",
+  },
+  {
+    image: kavyaMam,
+    name: "Kavya Gauri",
+    post: "Creative Head",
   },
   {
     image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
+    name: "Abhinav Mishra",
+    post: "Webmaster",
   },
   {
-    image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
-  },
-  {
-    image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
-  },
-  {
-    image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
-  },
-  {
-    image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
-  },
-  {
-    image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
-  },
-  {
-    image: abhinavSir,
-    name: "Jane Smith",
-    post: "Project Manager",
+    image: vanishaMam,
+    name: "Vanisha Agarwal",
+    post: "Campus Outreach Head",
   },
 ];
 
